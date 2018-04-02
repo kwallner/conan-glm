4376b4aa70f11ddf0371a2e764542342084f831b
Uilian Ries <uilianries@gmail.com>
2018-04-02 19:24:29 -0300

    Apply Bincrafters template
    - Use GLM example on test package
    - Set package as header-only
    - Build only one job by compiler
    - Copy license from downloaded package
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 | Bintray | Windows | Linux & macOS |
 |:--------:|:---------:|:-----------------:|
-|BINTRAY_BADGE_URL|APPVEYOR_BADGE_URL|TRAVIS_BADGE_URL|
+|[![Download](https://api.bintray.com/packages/bincrafters/public-conan/glm%3Abincrafters/images/download.svg) ](https://bintray.com/bincrafters/public-conan/glm%3Abincrafters/_latestVersion)|[![Build status](https://ci.appveyor.com/api/projects/status/github/bincrafters/conan-glm?svg=true)](https://ci.appveyor.com/project/BinCrafters/conan-sqlite-orm)|[![Build Status](https://travis-ci.org/bincrafters/conan-glm.svg)](https://travis-ci.org/bincrafters/conan-glm)|
 
 ## Conan.io Information
 
--- conanfile.py
+++ conanfile.py
@@ -7,28 +7,25 @@ import os
 class GlmConan(ConanFile):
     name = "glm"
     version = "0.9.8.5"
-    description="OpenGL Mathematics (GLM)"
-    url="https://github.com/bincrafters/conan-glm"
-    homepage="https://github.com/g-truc/glm"
+    description = "OpenGL Mathematics (GLM)"
+    url = "https://github.com/bincrafters/conan-glm"
+    homepage = "https://github.com/g-truc/glm"
+    author = "Bincrafters <bincrafters@gmail.com>"
     license = "MIT"
-    exports = "lib_licenses/*"
-    exports_sources = ["FindGLM.cmake"]
-    generators = "txt"
+    exports = ["FindGLM.cmake", "LICENSE.md"]
     source_subfolder = "source_subfolder"
+    no_copy_source = True
 
     def source(self):
         source_url = "https://github.com/g-truc/glm"
-        tools.get("{0}/archive/{1}.tar.gz".format(source_url,
-                                                          self.version))
+        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
         extracted_dir = "glm-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
+        self.patch_gcc()
 
-    def build(self):
-        # Hack that fixes bug that crashes gcc 7.3 - check if still necessary
-        # after glm 0.9.8.5
-        tools.replace_in_file(os.path.join(self.source_subfolder, "glm", "simd",\
-                                           "platform.h"),\
-                                           """#	elif (__GNUC__ == 7) && (__GNUC_MINOR__ == 2)
+    def patch_gcc(self):
+        header_file = os.path.join(self.source_subfolder, "glm", "simd", "platform.h")
+        tools.replace_in_file(header_file, """#	elif (__GNUC__ == 7) && (__GNUC_MINOR__ == 2)
 #		define GLM_COMPILER (GLM_COMPILER_GCC72)""",\
                                            """
 #	elif (__GNUC__ == 7) && (__GNUC_MINOR__ == 2)
@@ -36,11 +33,11 @@ class GlmConan(ConanFile):
 #	elif (__GNUC__ == 7) && (__GNUC_MINOR__ == 3)
 #		define GLM_COMPILER (GLM_COMPILER_GCC72)
                                            """)
-        self.output.info("No compilation necessary for GLM")
 
     def package(self):
-        self.copy("FindGLM.cmake", ".", ".")
-        self.copy("*",src=os.path.join(self.source_subfolder, "glm"),\
-                  dst=os.sep.join([".", "include", "glm"]))
-        self.copy(os.path.join("lib_licenses", "license*"), dst="licenses",
-                  ignore_case=True, keep_path=False)
+        self.copy("FindGLM.cmake")
+        self.copy("*", src=os.path.join(self.source_subfolder, "glm"), dst=os.path.join("include", "glm"))
+        self.copy("copying.txt", dst="licenses", src=self.source_subfolder)
+
+    def package_id(self):
+        self.info.header_only()
